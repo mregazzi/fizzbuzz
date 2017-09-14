d1e89c218594177209512c44ac3144abaa0ccd68
mregazzi <mregazzi@tiscali.it>
2017-09-14 16:39:54 +0200

    First commit

---
+++ MavenCucumberTest/feature/checkout.feature
@@ -0,0 +1,8 @@
+
+Feature: Checkout
+	I want to use this template for my feature file
+
+Scenario: Checkout a banana
+Given the price of a "banana" is 40c
+When I checkout 1 "banana"
+Then the total price should be 40c
---
+++ MavenCucumberTest/src/main/java/TestTraining/MavenCucumberTest/App.java
@@ -0,0 +1,13 @@
+package TestTraining.MavenCucumberTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
